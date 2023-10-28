5a9bbf3fe351896d860ec7876673104758971b1e
Guilherme Salgueiro Silveira <127560908+guilhermesilveiras@users.noreply.github.com>
2023-10-28 11:23:46 -0300

    Novo teste

--- Teste.mjs
+++ Teste.mjs
@@ -8,3 +8,11 @@ const p1 = new Produto("Air Jordan", 349.90, 10);
 const v1 = new Venda();
 let notinha = v1.emitirNota(p1, c1);
 console.log(notinha);
+
+const c2 = new Cliente("Pedro", "Rua 2323", "2345-6789", "maria@gmail.com");
+var nomeCliente2 = c2.getNome();
+const p2 = new Produto("Camisa de Time", 99.90, 5);
+const v2 = new Venda();
+v2.registroDeVenda(p2);
+let notinha2 = v2.emitirNota(p2, c2);
+console.log(notinha2);
